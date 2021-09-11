efe8af30318180f3da2ec0011a56ced2ece61593
Bhavanamonika <bhavana.monika23@gmail.com>
2021-09-11 16:53:18 +0530

    Updated

--- Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
+++ Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
@@ -3,7 +3,7 @@ public class Samplecode {
 
 	public static void main(String[] args) {
 		System.out.println("I am commiting the code to GIT");
-        System.out.println("Tesing CIC");
+        System.out.println("Testing CIC");
         System.out.println("Executing changes in jenkins automatically ");
         System.out.println("Poll SCM");
         System.out.println("Testing with Jenkins");
