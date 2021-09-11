6c0ab779167148d89bc446eb751c395704ed6f1b
Bhavanamonika <bhavana.monika23@gmail.com>
2021-09-11 16:51:16 +0530

    Committing again

--- Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
+++ Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
@@ -4,9 +4,9 @@ public class Samplecode {
 	public static void main(String[] args) {
 		System.out.println("I am commiting the code to GIT");
         System.out.println("Tesing CIC");
-        System.out.println("Testing Weirdo");
         System.out.println("Executing changes in jenkins automatically ");
         System.out.println("Poll SCM");
+        System.out.println("Testing with Jenkins");
 	}
 
 }
