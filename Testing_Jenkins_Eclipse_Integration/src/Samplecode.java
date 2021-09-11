8242b4c2a3e56f2c49bea71cb5cf73e15e5a3c8b
Bhavanamonika <bhavana.monika23@gmail.com>
2021-09-11 16:44:58 +0530

    Poll SCM

--- Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
+++ Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
@@ -5,7 +5,8 @@ public class Samplecode {
 		System.out.println("I am commiting the code to GIT");
         System.out.println("Tesing CIC");
         System.out.println("Testing Weirdo");
-
+        System.out.println("Executing changes in jenkins automatically ");
+        System.out.println("Poll SCM");
 	}
 
 }
