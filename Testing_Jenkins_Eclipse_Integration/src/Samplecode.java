b365fa9a7d27a88196b0dcd87ce01128c6fb16d9
Bhavanamonika <bhavana.monika23@gmail.com>
2021-09-11 16:00:00 +0530

    updated

--- Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
+++ Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
@@ -4,6 +4,7 @@ public class Samplecode {
 	public static void main(String[] args) {
 		System.out.println("I am commiting the code to GIT");
         System.out.println("Tesing CIC");
+        System.out.println("Testing Weirdo");
 
 	}
 
