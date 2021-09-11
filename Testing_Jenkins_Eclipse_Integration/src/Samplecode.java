32b668f896a868f8a80919260cb1c2ba5c26122d
Bhavanamonika <bhavana.monika23@gmail.com>
2021-09-11 15:51:36 +0530

    Commiting code

---
+++ Testing_Jenkins_Eclipse_Integration/src/Samplecode.java
@@ -0,0 +1,10 @@
+
+public class Samplecode {
+
+	public static void main(String[] args) {
+		System.out.println("I am commiting the code to GIT");
+        System.out.println("Tesing CIC");
+
+	}
+
+}
